9278a6dce05d2c2c99d8ca983caa08f8336829df
Maxime Princelle <maxime@princelle.org>
2022-01-21 17:59:54 +0100

    🐛 fix bug with envs on android

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ import 'pages/_layout.dart';
 
 Future main() async {
   // Load the .env file
-  await dotenv.load(fileName: ".env");
+  await dotenv.load(fileName: "envs");
 
   // Start the app
   runApp(const MyApp());
